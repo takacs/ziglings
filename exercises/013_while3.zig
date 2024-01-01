77e9223ba024d57aa990b19cc7adc87c362355bd
takacs <davidetkcs@gmail.com>
2024-01-01 18:17:25 +0100

    add 011-020 solutions

--- exercises/013_while3.zig
+++ exercises/013_while3.zig
@@ -22,10 +22,10 @@ pub fn main() void {
     // I want to print every number between 1 and 20 that is NOT
     // divisible by 3 or 5.
     while (n <= 20) : (n += 1) {
+        if (n % 3 == 0) continue;
+        if (n % 5 == 0) continue;
         // The '%' symbol is the "modulo" operator and it
         // returns the remainder after division.
-        if (n % 3 == 0) ???;
-        if (n % 5 == 0) ???;
         std.debug.print("{} ", .{n});
     }
 
